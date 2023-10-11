11e965a8a817889b4748c8fc1eb0d68b2f42fa7c
Khalil <Khalil.haouas@gmail.com>
2023-10-12 01:07:32 +0200

    data base connected

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,17 @@
 import express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
 
+dotenv.config()
+
+const dbConnect = ()=>{
+    try{
+        mongoose.connect(process.env.MONGO)
+        console.log("data base is connected")
+
+    }catch (error){console.log(error)}
+}
+dbConnect()
 const app = express()
 const PORT = 4000
 app.listen(PORT,()=> console.log(`Server is running an PORT ${PORT}`))
